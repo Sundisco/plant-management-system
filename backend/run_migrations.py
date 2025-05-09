980faa6284602284381aed04879037250f28befc
Sune <sune-s@hotmail.com>
2025-05-09 19:47:09 +0200

    Fix migration import: Update add_wind_speed import to use correct function name 'upgrade'

--- backend/run_migrations.py
+++ backend/run_migrations.py
@@ -1,4 +1,4 @@
-from app.database.migrations.add_wind_speed import add_wind_speed_column
+from app.database.migrations.add_wind_speed import upgrade as add_wind_speed_column
 from app.database.migrations.add_default_user import add_default_user
 from app.database.migrations.ensure_schema import ensure_schema
 from app.database.migrations.prepare_data import prepare_data
